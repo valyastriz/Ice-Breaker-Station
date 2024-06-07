40c51d77dac17d29aa0494def4c205aec54dfcab
valyastriz <valyastriz@gmail.com>
2024-06-06 22:22:56 -0400

    fixed URL is uselessFactClick function

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,34 @@
+
 const uselessFactEl = document.getElementById('uselessFact');
 let uselessFact;
 
 function uselessFactClick(event) {
     event.preventDefault();
-    fetch(`https://useless-facts-api-endpoint.com/api/v2/facts/random`)
-    .then(function(resp) {
-        return resp.json();
-    })
-    .then(function(data) {
-        console.log(data);
-        if (!data || !data.text) {
-            console.log('No results returned');
-            return;
-        }
-        uselessFact = data.text;
-        console.log('Random Useless Fact: ', uselessFact);
-        return;
-    })
-    .catch(function(error) {
-        console.error('Error fetching random useless fact: ', error);
+    let fact = '';
+    axios.get(`https://uselessfacts.jsph.pl/api/v2/facts/random`).then(resp => 
+    {
+        fact = resp.data.text;
+        console.log(fact);
     });
+
+    
+    // fetch(`https://cors-anywhere.herokuapp.com/https://useless-facts-api.herokuapp.com/api/v2/facts/random`)
+    // .then(function(resp) {
+    //     return resp.json();
+    // })
+    // .then(function(data) {
+    //     console.log(data);
+    //     if (!data || !data.text) {
+    //         console.log('No results returned');
+    //         return;
+    //     }
+    //     uselessFact = data.text;
+    //     console.log('Random Useless Fact: ', uselessFact);
+    //     return;
+    // })
+    // .catch(function(error) {
+    //     console.error('Error fetching random useless fact: ', error);
+    // });
 }
+
+uselessFactEl.addEventListener('click', uselessFactClick);
