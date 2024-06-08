f40e3f4947085165bd5e4f91020e36e582726761
valyastriz <valyastriz@gmail.com>
2024-06-08 19:04:58 -0400

    queried dom for id for where to display the results. Added function to checkFactType in local storage and then render uselessFact to the proper div once found

--- assets/search-results.js
+++ assets/search-results.js
@@ -1,10 +1,25 @@
+const quoteElement = document.getElementById('quoteDisplay');
+const factElement = document.getElementById('factDisplay');
+const jokeElement = document.getElementById('jokeDisplay')
+
+//render useless fact
+function renderUselessFact(data) {
+    document.getElementById('factDisplay').innerText = `$(data)`;
+}
+
+function checkFactType() {
+    const selectedType = localStorage.getItem('selectedType');
+    const data = localStorage.getItem(selectedType);
+
+    if (selectedType === 'uselessFact') {
+        renderUselessFact(data);
+    }
+
+    else {
+        console.error("Unknown Type");
+    }
+}
+
 document.addEventListener('DOMContentLoaded', (event) => {
-    const quoteElement = document.getElementById('quoteDisplay');
-    const factElement = document.getElementById('factDisplay');
-    const jokeElement = document.getElementById('jokeDisplay')
-
-    //check for useless fact
-    
-
-
-})
+    checkFactType();
+});
