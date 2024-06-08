bae5c87b377b1a4bbb87e89e062e12a9e9fed732
valyastriz <valyastriz@gmail.com>
2024-06-08 19:31:14 -0400

    added id to index.html div to make it more usefull univserally. updated renderuseless fact function create new html elements specificto useless fact

--- assets/search-results.js
+++ assets/search-results.js
@@ -1,17 +1,19 @@
-const quoteElement = document.getElementById('quoteDisplay');
-const factElement = document.getElementById('factDisplay');
-const jokeElement = document.getElementById('jokeDisplay');
+const displayEl = document.getElementById('displayResult');
 
 //render useless fact
 function renderUselessFact(data) {
-    factElement.innerText = `${data}`;
+    const factDiv = document.createElement('div');
+    const factEl = document.createElement('h1');
+    factEl.classList.add('text-white', 'text-2xl', 'text-slate-50', 'tracking-wide', 'font-bold');
+    factEl.innerText = `${data}`;
+    factDiv.appendChild(factEl);
+    displayEl.appendChild(factDiv);
 }
 
 function checkFactType() {
     const selectedType = localStorage.getItem('selectedType');
     const data = JSON.parse(localStorage.getItem(selectedType));
     if (selectedType === 'uselessFact') {
-        
         renderUselessFact(data.text);
     }
     else {
